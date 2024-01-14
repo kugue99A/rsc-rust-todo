4fcd832486a3c0812e4d371ab5a3ca5e75769a8c
kugue99A <hisabisa99yohey@gmail.com>
2024-01-14 17:06:05 +0900

    [add] sqlx

---
+++ front/rsc-project/prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  useTabs: false,
+}
+
+module.exports = config
--- front/rsc-project/src/app/page.tsx
+++ front/rsc-project/src/app/page.tsx
@@ -1,45 +1,48 @@
-import { Card, TreeView } from "@/components/common";
-import Link from "next/link";
-import { TreeViewContent } from "./_components";
+import { Card, TreeView } from '@/components/common'
+import Link from 'next/link'
+import { TreeViewContent } from './_components'
 
 export default function Home() {
-  const treeContent = {
-    id: 1,
-    name: 'node 1',
-    children: [
-      {
-        id: 11,
-        name: 'node 11',
-        children: [{
-          id: 111,
-          name: 'node 111',
-        }]
-      },
-      {
-        id: 12,
-        name: 'node 12',
+    const treeContent = {
+        id: 1,
+        name: 'node 1',
         children: [
-          {
-            id: 121,
-            name: 'node 121',
-          },
-          {
-            id: 122,
-            name: 'node 122',
-          }
-        ]
-      },
-    ]
-  }
+            {
+                id: 11,
+                name: 'node 11',
+                children: [
+                    {
+                        id: 111,
+                        name: 'node 111',
+                    },
+                ],
+            },
+            {
+                id: 12,
+                name: 'node 12',
+                children: [
+                    {
+                        id: 121,
+                        name: 'node 121',
+                    },
+                    {
+                        id: 122,
+                        name: 'node 122',
+                    },
+                ],
+            },
+        ],
+    }
 
-  return (
-    <div>
-      <h1>This is Home!!!</h1>
-      <Link href="/calendar">Go to Calendar</Link>
-      <Link href="/tasks">Go to Tasks</Link>
-      <Card><h2>test test</h2>
-      <TreeView markup={TreeViewContent} item={treeContent}/>
-      </Card>
-    </div>
+    return (
+        <div>
+            <h1>This is Home!!!</h1>
+            <Link href="/calendar">Go to Calendar</Link>
+            <Link href="/tasks">Go to Tasks</Link>
+            <Card>
+                <h2>test test</h2>
+                <TreeView markup={TreeViewContent} item={treeContent} />
+            </Card>
+        </div>
     )
 }
