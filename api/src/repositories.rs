753ee56237bd4eceafdc71b8d389526b129c466a
kugue99A <hisabisa99yohey@gmail.com>
2024-01-27 22:03:37 +0900

    [add] sqlx

--- api/src/repositories.rs
+++ api/src/repositories.rs
@@ -6,6 +6,7 @@ use anyhow::Ok;
 use::serde::{Deserialize, Serialize};
 use::thiserror::Error;
 use:: anyhow::Context;
+use validator::Validate;
 
 #[derive(Debug, Error)]
 enum RepositoryError {
@@ -29,8 +30,10 @@ pub struct Todo {
     pub completed: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct CreateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     text: String,
 }
 
@@ -41,8 +44,10 @@ impl CreateTodo {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct UpdateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     text: Option<String>,
     completed: Option<bool>,
 }
