56da75bb2224f2c2c9f64fa05e1db962155f2269
kugue99A <hisabisa99yohey@gmail.com>
2024-01-29 22:23:12 +0900

    [mod] await test

--- api/src/main.rs
+++ api/src/main.rs
@@ -95,7 +95,7 @@ mod test {
     async fn should_find_todo() {
       let expected = Todo::new(1, "should_find_todo".to_string());
       let repository = TodoRepositoryForMemory::new();
-      repository.create(CreateTodo::new("should_find_todo".to_string()));
+      repository.create(CreateTodo::new("should_find_todo".to_string())).await.expect("failed create todo");
       let req = build_todo_req_with_empty(Method::GET, "/todos/1");
       let res = create_app(repository).oneshot(req).await.unwrap();
       let todo = res_to_todo(res).await;
@@ -106,7 +106,7 @@ mod test {
     async fn should_get_all_todos() {
       let expected = Todo::new(1, "should_get_all_todos".to_string());
       let repository = TodoRepositoryForMemory::new();
-      repository.create(CreateTodo::new("should_get_all_todos".to_string()));
+      repository.create(CreateTodo::new("should_get_all_todos".to_string())).await.expect("failed create todo");
       let req = build_todo_req_with_empty(Method::GET, "/todos");
       let res = create_app(repository).oneshot(req).await.unwrap();
       let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
@@ -119,7 +119,7 @@ mod test {
     async fn should_update_todo() {
       let expected = Todo::new(1, "should_update_todo".to_string());
       let repository = TodoRepositoryForMemory::new();
-      repository.create(CreateTodo::new("before_update_todo".to_string()));
+      repository.create(CreateTodo::new("before_update_todo".to_string())).await.expect("failed create todo");
       let req = build_todo_req_with_json("/todos/1", Method::PATCH, r#"{
         "id": 1,
         "text": "should_update_todo",
@@ -133,7 +133,7 @@ mod test {
     #[tokio::test]
     async fn should_delete_todo() {
       let repository = TodoRepositoryForMemory::new();
-      repository.create(CreateTodo::new("should_delete_todo".to_string()));
+      repository.create(CreateTodo::new("should_delete_todo".to_string())).await.expect("failed create todo");
       let req = build_todo_req_with_empty(Method::DELETE, "/todos/1");
       let res = create_app(repository).oneshot(req).await.unwrap();
       assert_eq!(StatusCode::NO_CONTENT, res.status());
