78091afc38d0acbc52269062f1241f4c6c0048b9
kugue99A <hisabisa99yohey@gmail.com>
2024-07-27 20:32:01 +0900

    wip: add migrate

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -1,53 +1,58 @@
 use axum::{
-	extract::{Extension, Path},
-	http::StatusCode,
-	response::IntoResponse,
-	Json
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
 };
 use std::sync::Arc;
 
 use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
 
 pub async fn create_todo<T: TodoRepository>(
-	Json(payload): Json<CreateTodo>,
-	Extension(repository): Extension<Arc<T>>,
- ) -> Result<impl IntoResponse, StatusCode> {
-	let todo = repository
-		.create(payload)
-		.await
-		.or(Err(StatusCode::NOT_FOUND))?;
-	Ok((StatusCode::CREATED, Json(todo)))
+    Json(payload): Json<CreateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .create(payload)
+        .await
+        .or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::CREATED, Json(todo)))
 }
 
 pub async fn find_todo<T: TodoRepository>(
-	Path(id): Path<i32>,
-	Extension(repository): Extension<Arc<T>>,
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
 ) -> Result<impl IntoResponse, StatusCode> {
-	let todo = repository.find(id).await.or(Err(StatusCode::NOT_FOUND))?;
-	Ok((StatusCode::OK, Json(todo)))
+    let todo = repository.find(id).await.or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::OK, Json(todo)))
 }
 
 pub async fn all_todo<T: TodoRepository>(
-	Extension(repository): Extension<Arc<T>>,
+    Extension(repository): Extension<Arc<T>>,
 ) -> Result<impl IntoResponse, StatusCode> {
-	let todo = repository.all().await.unwrap();
-	Ok((StatusCode::OK, Json(todo)))
+    let todo = repository.all().await.unwrap();
+    Ok((StatusCode::OK, Json(todo)))
 }
 
 pub async fn update_todo<T: TodoRepository>(
-	Path(id): Path<i32>,
-	Json(payload): Json<UpdateTodo>,
-	Extension(repository): Extension<Arc<T>>,
+    Path(id): Path<i32>,
+    Json(payload): Json<UpdateTodo>,
+    Extension(repository): Extension<Arc<T>>,
 ) -> Result<impl IntoResponse, StatusCode> {
-	let todo = repository.update(id, payload)
-		.await
-		.or(Err(StatusCode::NOT_FOUND))?;
-	Ok((StatusCode::CREATED, Json(todo)))
+    let todo = repository
+        .update(id, payload)
+        .await
+        .or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::CREATED, Json(todo)))
 }
 
 pub async fn delete_todo<T: TodoRepository>(
-	Path(id): Path<i32>,
-	Extension(repository): Extension<Arc<T>>,
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
 ) -> StatusCode {
-	repository.delete(id).await.map(|_| StatusCode::NO_CONTENT).unwrap_or(StatusCode::NOT_FOUND)
+    repository
+        .delete(id)
+        .await
+        .map(|_| StatusCode::NO_CONTENT)
+        .unwrap_or(StatusCode::NOT_FOUND)
 }
